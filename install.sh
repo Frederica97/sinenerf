b2e92e8656207366943b60c1c68fa618da667b3b
Zinan Chi <31479492+Frederica97@users.noreply.github.com>
2023-11-24 14:53:47 -0500

    init

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip3 install torch torchvision torchaudiopip install pytorch_lightning==1.5.10 lpips test-tube
+pip install opencv-python matplotlib kornia
+pip install -U scikit-learn
+pip install omegaconf addict einops faiss-gpu opencv-python imageio-ffmpeg
+pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
