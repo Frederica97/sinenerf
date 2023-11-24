bab113466abd3d4e7f6af3c7f456bd1d1823bad0
Zinan Chi <31479492+Frederica97@users.noreply.github.com>
2023-11-24 15:09:25 -0500

    update torch version

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install torch==1.11.0+cu111 torchvision==0.11.2+cu111 -f https://download.pytorch.org/whl/cu111/torch_stable.html
 pip install opencv-python matplotlib kornia
 pip install -U scikit-learn
 pip install omegaconf addict einops faiss-gpu opencv-python imageio-ffmpeg
